dd2ed5a3380b5092f322436d8a9ceb11e1c99709
Higa <codehiga@gmail.com>
2023-02-07 09:37:46 -0300

    feat: aluno entity add

---
+++ src/domain/entities/aluno.ts
@@ -0,0 +1,68 @@
+import { Either, left, right } from '../../shared/either'
+import { ErrorResponse } from '../../shared/error-response'
+
+export interface AlunoProps {
+  id: string
+  nome: string
+  email: string
+  turma: string
+}
+
+interface ValidateAlunoPropsResponse {
+  campo?: string
+  erro?: string
+  valido: boolean
+}
+
+export class Aluno {
+  private constructor(private readonly _props: AlunoProps) {
+  }
+
+  static create(props: AlunoProps): Either<ErrorResponse, Aluno> {
+    const propsValidas = this.validate(props)
+    if(!propsValidas.valido) {
+      return left({
+        msg: propsValidas.erro
+      })
+    }
+    return right(new Aluno(props))
+  }
+
+  get props() {
+    return this._props
+  }
+
+  static validate({ id, nome, email }: AlunoProps): ValidateAlunoPropsResponse {
+    if(nome.length < 3 || nome.length > 64) {
+      return {
+        campo: 'nome',
+        erro: 'A quantidade de caracteres no NOME deve ser entre 3 e 64',
+        valido: false
+      }
+    }
+    if(email.length < 3 || email.length > 64) {
+      return {
+        campo: 'email',
+        erro: 'A quantidade de caracteres no EMAIL deve ser entre 3 e 64',
+        valido: false
+      }
+    }
+    if(!email.includes('@') || !email.includes('.')) {
+      return {
+        campo: 'email',
+        erro: 'Por favor forneça um e-mail válido',
+        valido: false
+      }
+    }
+    if(!id) {
+      return {
+        campo: 'id',
+        erro: 'É obrigatório haver um ID para o aluno',
+        valido: false
+      }
+    }
+    return {
+      valido: true
+    }
+  }
+}
