ef83fa111700775995cc6f2e81fca7a3d0f08a0c
Higa <codehiga@gmail.com>
2023-02-11 13:18:57 -0300

    feat: novo-aluno route

---
+++ src/main/routes/aluno-routes.ts
@@ -0,0 +1,7 @@
+import { adaptRoute } from "../adapters/express-route-adapter";
+import { makeNovoAlunoController } from "./../factories/novo-aluno-controller-factory";
+import { Router } from "express";
+
+export default (router: Router) => {
+  router.post("/novo-aluno", adaptRoute(makeNovoAlunoController()));
+};
