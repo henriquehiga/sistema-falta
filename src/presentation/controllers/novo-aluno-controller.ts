a65090b70485b86275b548a19f66e06c90ca8f51
Higa <codehiga@gmail.com>
2023-02-10 23:28:31 -0300

    feat: novo aluno controller presentation impl

---
+++ src/presentation/controllers/novo-aluno-controller.ts
@@ -0,0 +1,22 @@
+import { NovoAlunoUsecaseProtocol } from "./../../domain/usecases/protocols/novo-aluno-usecase-protocol";
+import { Controller } from "../protocols/controller";
+import { HttpRequest, HttpResponse } from "../protocols/http";
+
+export class NovoAlunoController implements Controller {
+  constructor(private readonly usecase: NovoAlunoUsecaseProtocol) {}
+
+  async handle(data: HttpRequest): Promise<HttpResponse> {
+    const { body } = data;
+    const response = await this.usecase.execute(body);
+    if (response.isLeft()) {
+      return {
+        body: response.value.msg,
+        statusCode: 400,
+      };
+    }
+    return {
+      body: null,
+      statusCode: 201,
+    };
+  }
+}
