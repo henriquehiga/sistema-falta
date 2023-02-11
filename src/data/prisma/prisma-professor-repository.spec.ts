70ebb0d95ae89f39b919986bd94e07ea5cb77877
Higa <codehiga@gmail.com>
2023-02-10 23:15:31 -0300

    test: professor prisma repository

---
+++ src/data/prisma/prisma-professor-repository.spec.ts
@@ -0,0 +1,40 @@
+import { PrismaProfessorRepository } from "./prisma-professor-repository";
+import { ProfessorRepository } from "./../professor-repository";
+import { ProfessorModel } from "../../domain/entities/models/professor-model";
+import { test, beforeEach, expect } from "vitest";
+import { prisma } from "../../main/infra/prisma/prisma";
+
+beforeEach(async () => {
+  await prisma.professor.deleteMany();
+});
+
+test("Espero salvar novo professor", async () => {
+  const professor: ProfessorModel.Model = {
+    id: "valid-uuid",
+    email: "validemail@mail.com",
+    nome: "valid name",
+    disciplina: "valid class",
+  };
+  const repository: ProfessorRepository = new PrismaProfessorRepository();
+  const professorCriado = await repository.salvar(professor);
+  expect(professorCriado.id).toBe("valid-uuid");
+});
+
+test("Espero lançar erro caso professor já exista", async () => {
+  const professor: ProfessorModel.Model = {
+    id: "valid-uuid",
+    email: "validemail@mail.com",
+    nome: "valid name",
+    disciplina: "valid class",
+  };
+
+  const professorDuplicado: ProfessorModel.Model = {
+    id: "valid-uuid",
+    email: "validemail@mail.com",
+    nome: "valid name",
+    disciplina: "valid class",
+  };
+  const repository: ProfessorRepository = new PrismaProfessorRepository();
+  await repository.salvar(professor);
+  await expect(repository.salvar(professorDuplicado)).rejects.toThrow();
+});
