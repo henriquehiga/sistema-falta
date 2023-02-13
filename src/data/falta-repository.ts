93a99aba228d145bd546d3ad48d4a4e5f887bcc1
Higa <codehiga@gmail.com>
2023-02-13 17:12:13 -0300

    feat: new method to redeem aluno by id

--- src/data/falta-repository.ts
+++ src/data/falta-repository.ts
@@ -3,4 +3,5 @@ import { FaltaModel } from "../domain/entities/models/falta-model";
 export interface FaltaRepository {
   salvar(data: FaltaModel.Model): Promise<FaltaModel.Model>;
   resgataPorId(id: string): Promise<FaltaModel.Model>;
+  resgataPorIdAluno(id: string): Promise<FaltaModel.Model[]>;
 }
