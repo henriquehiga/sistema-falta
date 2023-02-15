06dc5f8ae10cdb96c490eb50c6cc57275fe6e201
Higa <codehiga@gmail.com>
2023-02-15 09:17:04 -0300

    feat: qtd_aulas_semestre param

--- src/domain/entities/models/disciplina-model.ts
+++ src/domain/entities/models/disciplina-model.ts
@@ -2,6 +2,7 @@ export namespace DisciplinaModel {
   export type Model = {
     id: string;
     nome: string;
+    qtd_aulas_semestre: number;
   };
 
   export type Create = Omit<DisciplinaModel.Model, "id">;
