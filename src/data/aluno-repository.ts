2ed081015bc4d14b56e56903e3f7489487df30e3
Higa <codehiga@gmail.com>
2023-02-17 19:06:47 -0300

    fix: return null on resgataPorId

--- src/data/aluno-repository.ts
+++ src/data/aluno-repository.ts
@@ -2,5 +2,5 @@ import { AlunoModel } from "../domain/entities/models/aluno-model";
 
 export interface AlunoRepository {
   salvar(data: AlunoModel.Model): Promise<AlunoModel.Model>;
-  resgataPorId(id: string): Promise<AlunoModel.Model>;
+  resgataPorId(id: string): Promise<AlunoModel.Model | null>;
 }
