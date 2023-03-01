40bc454d9838c53ef071731b3c0d42a6ee8c2830
Letícia Silva <leticiasilva@Leticia-MacBook-Pro.local>
2023-02-28 21:22:56 -0300

    começo do front

--- src/view/src/App.tsx
+++ src/view/src/App.tsx
@@ -1,3 +1,12 @@
+import IMG from "./img/menino.jpg"
+
 export const App = () => {
-  return <div className="main-app">vini viado</div>;
+  return(
+    <div className="main-app">
+      ESCOLA OCTÓGONO
+      <div>LOGIN</div>
+      <img src={IMG} />
+    </div>
+  );
+  
 };
