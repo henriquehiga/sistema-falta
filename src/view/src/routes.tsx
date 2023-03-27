5ad5a8fee6177821574f4760067a2e67e4ad41b3
Henrique Higa <codehiga@gmail.com>
2023-03-27 19:49:40 -0300

    feat: rotas adicionadas para gerenciamento

--- src/view/src/routes.tsx
+++ src/view/src/routes.tsx
@@ -1,14 +1,21 @@
 import { Route, Routes } from "react-router-dom";
-import { DashboardProf } from "./pages/DashboardProf";
-import { DashboardTurma } from "./pages/DashboardTurma";
 import { Login } from "./pages/Login";
+import { GerenciamentoAluno } from "./pages/gerenciamento/GerenciamentoAluno";
+import { GerenciamentoMateria } from "./pages/gerenciamento/GerenciamentoMateria";
+import { GerenciamentoProfessor } from "./pages/gerenciamento/GerenciamentoProfessor";
+import { GerenciamentoTurma } from "./pages/gerenciamento/GerenciamentoTurma";
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route element={<Login />} path="/" />
-      <Route element={<DashboardProf />} path="/dashboard-professor" />
-      <Route element={<DashboardTurma />} path="/dashboard-turma" />
+      <Route
+        element={<GerenciamentoProfessor />}
+        path="/gerenciamento-professor"
+      />
+      <Route element={<GerenciamentoAluno />} path="/gerenciamento-aluno" />
+      <Route element={<GerenciamentoTurma />} path="/gerenciamento-turma" />
+      <Route element={<GerenciamentoMateria />} path="/gerenciamento-materia" />
     </Routes>
   );
 };
