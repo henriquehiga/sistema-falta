746aa13a81f07aacd6063720e392e4f5a56ee547
Henrique Higa <codehiga@gmail.com>
2023-03-27 19:13:06 -0300

    fix: move folder

--- src/view/src/App.tsx
+++ src/view/src/App.tsx
@@ -1,21 +1,9 @@
-import IMG from "./img/menino.jpg"
-import { DashboardAluno } from "./pages/DashboardAluno";
-import { DashboardProf } from "./pages/DashboardProf";
-import { DashboardTurma } from "./pages/DashboardTurma";
-import { GerenciamentoAluno } from "./pages/GerenciamentoAluno";
-import { DashboardAdicionarAluno } from "./pages/DashboardAdicionarAluno";
-import { GerenciamentoMateria } from "./pages/GerenciamentoMateria";
-import { DashboardAdicionarMateria } from "./pages/DashboardAdicionarMateria";
-import { GerenciamentoTurma } from "./pages/GerenciamentoTurma";
-import { DashboardAdicionarTurma } from "./pages/DashboardAdicionarTurma";
-import { GerenciamentoProfessor } from "./pages/GerenciamentoProfessor";
-import { DashboardAdicionarProfessor } from "./pages/DashboardAdicionarProfessor";
-import { Login } from "./pages/login";
+import { GerenciamentoAluno } from "./pages/gerenciamento/GerenciamentoAluno";
 
 export const App = () => {
-  return(
+  return (
     <div className="main-app">
-      <GerenciamentoAluno/>
+      <GerenciamentoAluno />
     </div>
   );
 };
