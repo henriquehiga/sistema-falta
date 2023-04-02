0ce768c62bed54e52ae49b0f22fa1c7b92db86ca
Henrique Higa <codehiga@gmail.com>
2023-04-02 03:16:36 -0300

    chore: removed front

--- src/view/src/main.tsx
+++
@@ -1,7 +0,0 @@
-import ReactDOM from "react-dom/client";
-import { App } from "./App";
-import "./styles/main.css";
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
-);
