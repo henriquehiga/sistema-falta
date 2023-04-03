c33dfa6fa43462a6b8aa86a17047b9008b53cdbd
Henrique Higa <codehiga@gmail.com>
2023-04-03 02:09:47 -0300

    fix: remove disiciplina

--- src/domain/entities/models/professor-model.ts
+++ src/domain/entities/models/professor-model.ts
@@ -2,7 +2,6 @@ export namespace ProfessorModel {
   export type Model = {
     id: string;
     nome: string;
-    disciplina_id: string;
   };
 
   export type Create = Omit<ProfessorModel.Model, "id">;
